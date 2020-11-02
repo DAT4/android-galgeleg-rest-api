94ea4c8dbcc23eeb2f3aada3c17c398ebbe9df02
Martin <mail@mama.sh>
2020-11-02 17:12:33 +0100

    HighScore implemented

--- database.go
+++ database.go
@@ -11,6 +11,15 @@ import (
 	"time"
 )
 
+type HighScore struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty"`
+	Word   Word               `json:"word"`
+	Player string             `json:"player"`
+	Time   int                `json:"time"`
+	Hints  int                `json:"hints"`
+	Wrongs int                `json:"wrongs"`
+}
+
 type Category struct {
 	Title string `json:"title"`
 	Words []Word `json:"words"`
@@ -25,6 +34,62 @@ type Word struct {
 	Hint2       string             `json:"hint2"`
 	Hint3       string             `json:"hint3"`
 	Category    string             `json:"category"`
+}
+
+func getHighScores() (scores []HighScore) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := client.Database("dtu").Collection("hangmanHighScore").Find(ctx, bson.M{})
+	var highScore HighScore
+	for cursor.TryNext(context.Background()) {
+		cursor.Decode(&highScore)
+		scores = append(scores, highScore)
+	}
+	defer client.Disconnect(ctx)
+	return scores
+}
+
+func addHighScore(highScore HighScore) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	_, err = client.Database("dtu").Collection("hangmanHighScore").InsertOne(ctx, highScore)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func deleteHighScore(score HighScore) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	filter := bson.M{"_id": score.ID}
+	_, err = client.Database("dtu").Collection("hangmanHighScore").DeleteOne(ctx, filter)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func getWords() (words []Word) {
@@ -131,10 +196,7 @@ func deleteWord(word Word) {
 		fmt.Println(err)
 	}
 	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-	filter := bson.M{
-		"word":     word.Word,
-		"category": word.Category,
-	}
+	filter := bson.M{"_id": word.ID}
 	_, err = client.Database("dtu").Collection("hangman").DeleteOne(ctx, filter)
 	if err != nil {
 		fmt.Println(err)
--- main.go
+++ main.go
@@ -9,26 +9,43 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/categories", listCategories).Methods("GET")
+	r.HandleFunc("/categories/", listCategories).Methods("GET")
 	r.HandleFunc("/", listWords).Methods("GET")
 	r.HandleFunc("/", addWord).Methods("POST")
 	r.HandleFunc("/", editWord).Methods("PUT")
 	r.HandleFunc("/", eraseWord).Methods("DELETE")
+	r.HandleFunc("/score", listScores).Methods("GET")
+	r.HandleFunc("/score", addScore).Methods("POST")
+	r.HandleFunc("/score", deleteScore).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":5050", r))
 }
+func listScores(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(getHighScores())
+}
+
 func listWords(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(getWords())
 }
+
 func listCategories(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(getCategories())
 }
+
 func addWord(w http.ResponseWriter, r *http.Request) {
 	var word Word
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewDecoder(r.Body).Decode(&word)
 	createWord(word)
+}
+
+func addScore(w http.ResponseWriter, r *http.Request) {
+	var score HighScore
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewDecoder(r.Body).Decode(&score)
+	addHighScore(score)
 }
 
 func editWord(w http.ResponseWriter, r *http.Request) {
@@ -37,9 +54,17 @@ func editWord(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&word)
 	updateWord(word)
 }
+
 func eraseWord(w http.ResponseWriter, r *http.Request) {
 	var word Word
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewDecoder(r.Body).Decode(&word)
 	deleteWord(word)
+}
+
+func deleteScore(w http.ResponseWriter, r *http.Request) {
+	var score HighScore
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewDecoder(r.Body).Decode(&score)
+	deleteHighScore(score)
 }
